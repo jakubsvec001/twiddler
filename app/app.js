b0c8f1371445a271777cafd3c5ed5e00467fafdb
Jake Wija <jakewija@icloud.com[core]>
2019-11-19 15:56:49 -0800

    ADD image links for users in data_generator.js
    CREATE simple container template for tweets with image, name, handle, time-ago text, and tweet content. Also, ADD a button at bottom of tweets to allow for dynamic pulling of new tweets.
    
    ADD simple css for containers.

--- app/app.js
+++ app/app.js
@@ -1,16 +1,14 @@
 $(document).ready(function(){
-  var $main = $('main');
-  $main.html('');
+  // var $contentBody = $('.content-body');
+  // $contentBody.html('');
 
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($main);
-    index -= 1;
-  }
+  // var index = streams.home.length - 1;
+  // while(index >= 0){
+  //   var tweet = streams.home[index];
+  //   var $tweet = $('<div></div>');
+  //   $tweet.text('@' + tweet.user + ': ' + tweet.message);
+  //   $tweet.appendTo($contentBody);
+  //   index -= 1;
+  // }
 
 });
-
-console.log('hello')
